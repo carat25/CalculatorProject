b2369187c568cbafc82ef62ba867cb034b6bed0d
Cara Jane Tran <carajanetran@Macbook-2.local>
2021-06-18 10:48:06 +1000

    initial commit

--- Calculator/main2.js
+++ Calculator/main2.js
@@ -7,17 +7,18 @@ const displayValue = (output) => {
   displayArea.innerHTML = output;
 }
 
-
 //this will attach any button values being clicked and will display in the output area
 const appendDisplayValue = (value) => {
   displayValue(`${displayArea.innerHTML} ${value}`);
 }
 
-let result = 0;
-let lastOperator = undefined;
-let clearOnNextDigit = false;
+//empty array to be used for storing button inputs
+let inputsArr = []; 
 
+//global function for clearing the display area
 const clearDisplayValue = () => displayValue("");
+
+
 
 //this is the function to be called once button/s are clicked
 const buttonClick = (event) => {
@@ -25,10 +26,9 @@ const buttonClick = (event) => {
   //Clearing display area
   if (buttonValue === "C") {
     clearDisplayValue();
-    result = 0;
-    clearOnNextDigit = false;
+    inputsArr = [];
   } else {
-    if (clearOnNextDigit) {
+    if (clearOnNextDigitInput) {
       clearDisplayValue();
     }
     appendDisplayValue(buttonValue);
@@ -40,54 +40,48 @@ for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", buttonClick);
 }
 
+const operators = ["+", "-", "/", "*"];
+let clearOnNextDigitInput = false;
+
 //this is the function to be called once operator/s are clicked
 const operatorButton = (event) => {
-  const operatorValue = event.target.value;
-  lastOperator = operatorValue;
   const currentValue = Number.parseInt(displayArea.innerHTML);
-  if (result === 0) {
-    result = currentValue;
-  } else {
-    if (operatorValue === "+") {
-      result += currentValue;
-      displayValue(currentValue);
-    } if (operatorValue === "*") {
-      result *= currentValue;
-      displayValue(currentValue);
-    } if (operatorValue === "-") {
-      result -= currentValue;
-      displayValue(currentValue);
-    } if (operatorValue === "/") {
-      result /= currentValue;
-      displayValue(currentValue);
-    } if (operatorValue === "=") {
-      displayValue(result);
-    }
+  inputsArr.push(currentValue);
 
+  const operatorValue = event.target.value;
+  inputsArr.push(operatorValue);
+
+  if (operatorValue === "=") {
+    const result = computeInputs(inputsArr);
+    displayValue(result);
+    inputsArr = [];
+  } else {
+    clearOnNextDigitInput = true;
   }
-  clearOnNextDigit = true;
+}
+
+//math operations
+const computeInputs = (inputs) => {
+  let lastOperator = undefined; 
+  return inputs.reduce((acc, currentValue) => {
+    if (operators.includes(currentValue)) {
+      lastOperator = currentValue;
+    } else if (lastOperator && !isNaN(currentValue)) {
+      if (lastOperator === "+") { 
+        acc += currentValue;
+      } if (lastOperator === "*") {
+        acc *= currentValue;
+      } if (lastOperator === "-") {
+        acc -= currentValue;
+      } if (lastOperator === "/") {
+        acc /= currentValue;
+      }
+    }
+    return acc;
+  });
 }
 
 //this will loop to all operator buttons and  add the click event
 for (let j = 0; j < operator.length; j++) {
   operator[j].addEventListener("click", operatorButton);
-}
-
-//Math operations
-
-
-const add = (n1, n2) => {
-  n1 + n2;
-}
-
-const multiply = (n1, n2) => {
-  n1 * n2;
-}
-
-const subtract = (n1, n2) => {
-  n1 - n2;
-}
-
-const divide = (n1, n2) => {
-  n1 / n2;
 }
